a8681f6883838e772c68f60d31331a9ac72bc808
andreazp <andreazamudiopalafox@gmail.com>
2017-06-26 16:53:03 -0400

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,13 +1,15 @@
 //Let's try git branch
 
-function Person(Andrea) {
+function Person(name) {
   this.name = name;
   this.greeting = function() {
     alert('Hi! I\'m ' + this.name + '.');
   };
 }
 
-var person1 = new Person('Andrea');
+var person1 = new Person('Steve');
+var person2 = new Person('Andrea');
 person1.greeting(); // 'Hi! I'm steve'
+person2.greeting();
 
 //add Jesus, Daniel, Andrea
